f6dd8c7416cf2fae1fc667adc89a09db1c38a086
Robert Moss <rmoss92@gmail.com>
2020-10-01 02:40:55 -0700

    RLInterface "dev" in test/deps.jl for Julia 1.0

--- test/deps.jl
+++ test/deps.jl
@@ -2,5 +2,7 @@ using Pkg
 
 if VERSION < v"1.1"
     pkg"dev https://github.com/JuliaPOMDP/POMDPs.jl"
+    pkg"dev https://github.com/JuliaPOMDP/RLInterface.jl"
+else
+    pkg"add https://github.com/JuliaPOMDP/RLInterface.jl"
 end
-pkg"add https://github.com/JuliaPOMDP/RLInterface.jl"
