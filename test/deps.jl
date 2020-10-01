0eaf69b7734a517fd7a8180ffbc2be53ac6f2c20
Robert Moss <rmoss92@gmail.com>
2020-10-01 03:00:50 -0700

    Travis Julia 1.0: "dev" POMDPs and RLInterface at the same time

--- test/deps.jl
+++ test/deps.jl
@@ -1,8 +1,7 @@
 using Pkg
 
 if VERSION < v"1.1"
-    pkg"dev https://github.com/JuliaPOMDP/POMDPs.jl"
-    pkg"dev https://github.com/JuliaPOMDP/RLInterface.jl"
+    pkg"dev https://github.com/JuliaPOMDP/POMDPs.jl https://github.com/JuliaPOMDP/RLInterface.jl"
 else
     pkg"add https://github.com/JuliaPOMDP/RLInterface.jl"
 end
